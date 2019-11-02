d3cef581a609d65a57ea69175bad0bc49ed64a31
Syt <3295539817@qq.com>
2019-11-02 16:27:36 +0800

    去除一部分注释。去除主函数的注释对于其他类new它没有影响。

--- src/mygui/friendslist2/TestDialogMenu.java
+++ src/mygui/friendslist2/TestDialogMenu.java
@@ -38,15 +38,15 @@ public class TestDialogMenu extends JDialog {
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		try {
-//			TestDialogMenu dialog = new TestDialogMenu();
-//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-//			dialog.setVisible(true);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static void main(String[] args) {
+		try {
+			TestDialogMenu dialog = new TestDialogMenu();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Create the dialog.
