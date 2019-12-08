8e7b8b1262ee38033a45a8ecc1898ebf15cc4a43
Syt <3295539817@qq.com>
2019-12-08 14:30:06 +0800

    引入并测试对JSON的支持

---
+++ src/test/JsonTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author xuxin
+ *
+ */
+public class JsonTest {
+
+	/**
+	 * 
+	 */
+	public JsonTest() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
