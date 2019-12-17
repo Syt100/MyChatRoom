5f86cd938472956798840834670a4b134aabff80
Syt <3295539817@qq.com>
2019-12-18 00:50:20 +0800

    优化代码结构

--- src/util/XMLOperation.java
+++ src/util/XMLOperation.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package util;
 
 import java.io.File;
@@ -17,31 +14,36 @@ import org.dom4j.io.XMLWriter;
 import bean.Users;
 
 /**
+ * XML文件的相关操作
  * @author xuxin
  *
  */
 public class XMLOperation {
 	String soucePath = "./rec/xml/Users.xml";
-	File file = new File(soucePath);
-	SAXReader read = new SAXReader();
-	Document doc;
 
 	public static void main(String[] arg0) {
-		new XMLOperation();
+		XMLOperation xml = new XMLOperation();
+		Users u = new Users("8899", "aiudnsv", "zzz", ",");
+		xml.addUser(u);
 	}
 
 	/**
-	 * 
+	 * 默认的无参构造方法
 	 */
 	public XMLOperation() {
-		// TODO 自动生成的构造函数存根
-		initDocument();
-		Users u = new Users("jhn", "zzz");
-		//addUser(u);
+
 	}
 
+	/**
+	 * 初始化Document
+	 * 
+	 * @return Document
+	 */
 	private Document initDocument() {
+		Document doc = null;
 		try {
+			File file = new File(soucePath);
+			SAXReader read = new SAXReader();
 			doc = read.read(file);
 		} catch (DocumentException e) {
 			// TODO 自动生成的 catch 块
@@ -50,39 +52,21 @@ public class XMLOperation {
 		return doc;
 	}
 
-	public static Element useDom4JReadXml(String soucePath) {
-		try {
-			File file = new File(soucePath);
-			SAXReader read = new SAXReader();
-			Document doc = read.read(file);
-			Element root = doc.getRootElement();
-			return root;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 	public void addUser(Users users) {
-		try {
-			Document doc = read.read(file);
-			Element root = doc.getRootElement();
-			if (isElementNameExit(users.getName())) {
-				System.out.println("我退出了");
-				return;
-			}
-
-			Element newUser = root.addElement("user").addAttribute("index", "4");
-			newUser.addElement("id").setText(users.getId());
-			newUser.addElement("name").setText(users.getName());
-			newUser.addElement("password").setText(users.getPassword());
-			newUser.addElement("friends").setText(users.getFriends());
-			writeXMLFile(doc);
-		} catch (DocumentException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
+		Document doc = initDocument();
+		Element root = doc.getRootElement();
+		if (isElementIdExit(users.getId())) {
+			System.out.println("该id已存在");
+			return;
 		}
+
+		Element newUser = root.addElement("user").addAttribute("state", "normal");
+		newUser.addElement("id").setText(users.getId());
+		newUser.addElement("name").setText(users.getName());
+		newUser.addElement("password").setText(users.getPassword());
+		newUser.addElement("friends").setText(users.getFriends());
+		writeXMLFile(doc);
+
 	}
 
 	/**
@@ -93,7 +77,7 @@ public class XMLOperation {
 	 */
 	public boolean isElementNameExit(String name1) {
 		boolean exit = false;
-		initDocument();
+		Document doc = initDocument();
 		Element root = doc.getRootElement();
 
 		Element foo;
@@ -109,10 +93,10 @@ public class XMLOperation {
 		}
 		return exit;
 	}
-	
+
 	public boolean isElementIdExit(String name1) {
 		boolean exit = false;
-		initDocument();
+		Document doc = initDocument();
 		Element root = doc.getRootElement();
 
 		Element foo;
@@ -128,12 +112,12 @@ public class XMLOperation {
 		}
 		return exit;
 	}
-	
+
 	public Users getUsersById(String id) {
-		if(!isElementIdExit(id)) {
+		if (!isElementIdExit(id)) {
 			return null;
 		}
-		initDocument();
+		Document doc = initDocument();
 		Element root = doc.getRootElement();
 
 		Element foo;
@@ -142,7 +126,8 @@ public class XMLOperation {
 			foo = (Element) iterator.next();
 
 			if (foo.elementText("id").equals(id)) {
-				Users user = new Users(foo.elementText("id"), foo.elementText("name"), foo.elementText("password"), foo.elementText("friends"));
+				Users user = new Users(foo.elementText("id"), foo.elementText("name"), foo.elementText("password"),
+						foo.elementText("friends"));
 				return user;
 			}
 		}
@@ -151,13 +136,12 @@ public class XMLOperation {
 
 	private boolean writeXMLFile(Document doc) {
 		boolean flag = false;
+		
 		try {
-			// 4
 			XMLWriter writer = new XMLWriter(OutputFormat.createPrettyPrint());
 			FileOutputStream fos = new FileOutputStream(soucePath);
 			writer.setOutputStream(fos);
 
-			// 5
 			writer.write(doc);
 			System.out.println("写出完毕!");
 			flag = true;
