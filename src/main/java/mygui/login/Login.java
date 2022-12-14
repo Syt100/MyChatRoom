6c6e5b46b2f59d0a8fb3e376c78cb9db6ea92aed
Syt <3295539817@qq.com>
2022-12-14 20:25:58 +0800

    优化代码格式

--- src/main/java/mygui/login/Login.java
+++ src/main/java/mygui/login/Login.java
@@ -8,19 +8,18 @@ import java.net.Socket;
 
 /**
  * 这个接口定义实现登录界面的方法
- * 
- * @author xuxin
  *
+ * @author xuxin
  */
 public interface Login {
 
-	/**
-	 * 从登录界面跳转到好友列表界面
-	 * 
-	 * @param user
-	 * @param socket
-	 * @param out
-	 * @param in
-	 */
-	public void skipToFriendList(Users user, Socket socket, PrintWriter out, BufferedReader in);
+    /**
+     * 从登录界面跳转到好友列表界面
+     *
+     * @param user   user
+     * @param socket socket
+     * @param out    out
+     * @param in     in
+     */
+    public void skipToFriendList(Users user, Socket socket, PrintWriter out, BufferedReader in);
 }
