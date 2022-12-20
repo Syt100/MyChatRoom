632ba1f5f4f57d750eccbbfee73b93b45d3630a2
Syt <3295539817@qq.com>
2022-12-20 16:55:17 +0800

    优化lambda

--- src/main/java/mygui/login/Login3.java
+++ src/main/java/mygui/login/Login3.java
@@ -24,19 +24,17 @@ public class Login3 {
 	private static final int DEFAULT_WIDTH=410;
     private static final int DEFAULT_HEIGHT=330;
     //private static final int DEFAULT_HEIGHT_EXTEND=500;
-    
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login3 window = new Login3();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		EventQueue.invokeLater(() -> {
+			try {
+				Login3 window = new Login3();
+				window.frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		});
 	}
@@ -64,7 +62,7 @@ public class Login3 {
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		frame.setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel_logo = new JPanel();
 		panel_logo.setBackground(Color.WHITE);
 		panel_logo.setBounds(0, 0, 401, 141);
@@ -118,7 +116,7 @@ public class Login3 {
 		comboBox.setEditable(true);
 		comboBox.setBounds(145, 12, 159, 22);
 		panel_operation.add(comboBox);
-		
+
 		passwordField_mima = new JPasswordField();
 		passwordField_mima.setBounds(145, 43, 159, 22);
 		panel_operation.add(passwordField_mima);
@@ -172,15 +170,12 @@ public class Login3 {
 					lbl_tips.setVisible(true);
 
 					// 用多线程实现让lbl_tips一段时间后消失
-					Thread t = new Thread(new Runnable() {
-						@Override
-						public void run() {
-							try {
-								Thread.sleep(3000);// 该线程睡眠3秒
-							} catch (InterruptedException ex) {
-							}
-							lbl_tips.setVisible(false);
+					Thread t = new Thread(() -> {
+						try {
+							Thread.sleep(3000);// 该线程睡眠3秒
+						} catch (InterruptedException ex) {
 						}
+						lbl_tips.setVisible(false);
 					});
 					t.start();// 启动线程
 					return;
@@ -188,7 +183,7 @@ public class Login3 {
 				if (account.equals(designate_account) && password_1.equals(designate_password)) {// 登录成功
 					//JOptionPane.showMessageDialog(null, "登录成功！");
 					layeredPane_operation.setLayer(panel_operation, -1);
-					
+
 					// 用计时器实现延迟
 					Timer timer = new Timer();// 实例化Timer类
 					timer.schedule(new TimerTask() {
@@ -201,7 +196,7 @@ public class Login3 {
 							this.cancel();
 						}
 					}, 1000);// 延迟x毫秒后启动
-					
+
 				} else {
 					//JOptionPane.showMessageDialog(null, "账号或密码错误，请重试！");
 					lbl_tips.setText("账号或密码错误，请重试！");
@@ -235,7 +230,7 @@ public class Login3 {
 				}
 			}
 		});
-		
+
 
 		JButton btn_erweima = new JButton("");// 显示二维码
 		btn_erweima.setIcon(new ImageIcon(Login3.class.getResource("/Images/QRcode.png")));
@@ -250,33 +245,31 @@ public class Login3 {
 		btn_zhucezhanghao.setContentAreaFilled(false);// 按钮透明
 		btn_zhucezhanghao.setBorderPainted(false);// 去除边框
 		panel_operation.add(btn_zhucezhanghao);
-		btn_zhucezhanghao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//注册账号事件
-				if(frame.getHeight()==DEFAULT_HEIGHT) {
-					contentPane.add(zhuCe());//绘制注册面板，并将其添加到contentPane中
-					setSizeExtend();//设置窗口变大，setSizeExtend()在下面具体实现
-				}else {
-					setSizeDefault();
-					contentPane.remove(zhuCe());
-				}			
+		btn_zhucezhanghao.addActionListener(e -> {
+			//注册账号事件
+			if(frame.getHeight()==DEFAULT_HEIGHT) {
+				contentPane.add(zhuCe());//绘制注册面板，并将其添加到contentPane中
+				setSizeExtend();//设置窗口变大，setSizeExtend()在下面具体实现
+			}else {
+				setSizeDefault();
+				contentPane.remove(zhuCe());
 			}
 		});
-		
-		
+
+
 		JPanel panel_landing = new JPanel();
 		panel_landing.setBackground(Color.WHITE);
 		layeredPane_operation.setLayer(panel_landing, 9);
 		panel_landing.setBounds(0, 0, 401, 153);
 		layeredPane_operation.add(panel_landing);
 		panel_landing.setLayout(null);
-		
+
 		JLabel lbl_landing_denglu = new JLabel("登录中...");
 		lbl_landing_denglu.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_landing_denglu.setFont(new Font("宋体", Font.PLAIN, 16));
 		lbl_landing_denglu.setBounds(119, 21, 162, 31);
 		panel_landing.add(lbl_landing_denglu);
-		
+
 		JButton btn_landing_quxiao = new JButton("取消");
 		btn_landing_quxiao.setFont(new Font("宋体", Font.PLAIN, 16));
 		btn_landing_quxiao.setBounds(144, 89, 113, 31);
@@ -297,34 +290,34 @@ public class Login3 {
 		panel_zhuce.setVisible(true);
 		panel_zhuce.setLayout(null);
 		panel_zhuce.setBorder(new TitledBorder(null, "用户注册", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		
+
 		JLabel lblNewLabel = new JLabel("用户名");
 		lblNewLabel.setBounds(66, 22, 70, 26);
 		panel_zhuce.add(lblNewLabel);
-		
+
 		JTextField textField = new JTextField();
 		textField.setBounds(146, 25, 163, 21);
 		panel_zhuce.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("密码");
 		lblNewLabel_1.setBounds(66, 54, 70, 26);
 		panel_zhuce.add(lblNewLabel_1);
-		
+
 		JTextField textField_1 = new JTextField();
 		textField_1.setBounds(146, 56, 163, 21);
 		panel_zhuce.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("确认密码");
 		lblNewLabel_2.setBounds(66, 84, 70, 26);
 		panel_zhuce.add(lblNewLabel_2);
-		
+
 		JTextField textField_2 = new JTextField();
 		textField_2.setBounds(146, 87, 163, 21);
 		panel_zhuce.add(textField_2);
 		textField_2.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("取消");
 		btnNewButton.setBounds(66, 120, 93, 23);
 		panel_zhuce.add(btnNewButton);
@@ -333,7 +326,7 @@ public class Login3 {
 				setSizeDefault();
 			}
 		});
-		
+
 		JButton btnNewButton_1 = new JButton("确认");
 		btnNewButton_1.setBounds(216, 120, 93, 23);
 		panel_zhuce.add(btnNewButton_1);
@@ -351,38 +344,38 @@ public class Login3 {
 		panel_zhaohui.setBackground(Color.WHITE);
 		panel_zhaohui.setBorder(new TitledBorder(null, "\u627E\u56DE\u5BC6\u7801", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_zhaohui.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("账号");
 		lblNewLabel.setBounds(80, 25, 54, 15);
 		panel_zhaohui.add(lblNewLabel);
-		
+
 		JTextField textField = new JTextField();
 		textField.setBounds(157, 25, 155, 21);
 		panel_zhaohui.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("电子邮件");
 		lblNewLabel_1.setBounds(80, 56, 54, 15);
 		panel_zhaohui.add(lblNewLabel_1);
-		
+
 		JTextField textField_1 = new JTextField();
 		textField_1.setBounds(157, 56, 155, 21);
 		panel_zhaohui.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("取消");
 		btnNewButton.setBounds(80, 87, 93, 23);
 		panel_zhaohui.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 			}
 		});
-		
+
 		JButton btnNewButton_1 = new JButton("找回");
 		btnNewButton_1.setBounds(219, 87, 93, 23);
 		panel_zhaohui.add(btnNewButton_1);
-		
+
 		return panel_zhaohui;
 	}
 
